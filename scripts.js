666ceb2d5baee2ae75d42fd39d9c67df2ba7096b
Jose Cano <cano.5@wright.edu>
2023-09-29 14:31:13 -0400

    Background color and basic math functions

---
+++ scripts.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return (a + b);
+}
+
+function subtract(minuend, subtrahend) {
+    return (minuend - subtrahend);
+}
+
+function multiply(a, b) {
+    return (a * b);
+}
+
+function divide(dividend, divisor) {
+    return (dividend / divisor);
+}
+
+function modulo(dividend, divisor) {
+    return (dividend % divisor);
+}
